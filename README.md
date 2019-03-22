8e75f6c567b764943f33d9c73dafc94e7223047f
Nhan Cao <nhancv92@gmail.com>
2019-03-23 02:02:09 +0700

    integrate opencv

---
+++ README.md
@@ -0,0 +1,16 @@
+
+## SETUP
+
+1. Download opencv sdk for android at: https://opencv.org/releases.html
+
+Direct link
+https://sourceforge.net/projects/opencvlibrary/files/4.0.1/opencv-4.0.1-android-sdk.zip/download
+
+2. Unzip opencv sdk and place it in the same root project folder
+
+```
+/Projects
+    /nc-android-facemask
+    /OpenCV-android-sdk
+```
+
--- app/src/main/java/com/nhancv/facemask/MainActivity.java
+++ app/src/main/java/com/nhancv/facemask/MainActivity.java
@@ -1,9 +1,24 @@
 package com.nhancv.facemask;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        OpenCVLoader.initDebug();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
