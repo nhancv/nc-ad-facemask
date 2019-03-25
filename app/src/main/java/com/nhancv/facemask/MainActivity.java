0973ae9424cd53521c19a6737aee18847e646cdd
Nhan Cao <nhancv92@gmail.com>
2019-03-25 18:21:21 +0700

    update

--- app/src/main/java/com/nhancv/facemask/MainActivity.java
+++ app/src/main/java/com/nhancv/facemask/MainActivity.java
@@ -3,13 +3,7 @@ package com.nhancv.facemask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import org.opencv.android.OpenCVLoader;
-
 public class MainActivity extends AppCompatActivity {
-
-    static {
-        OpenCVLoader.initDebug();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
