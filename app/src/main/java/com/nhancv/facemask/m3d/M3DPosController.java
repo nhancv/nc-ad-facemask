85e3e7787697dac6a33012e9b5e14334639dd7d6
phy lieng <phylieng97@gmail.com>
2019-03-28 17:14:02 +0700

    add transformation methods

--- app/src/main/java/com/nhancv/facemask/m3d/M3DPosController.java
+++ app/src/main/java/com/nhancv/facemask/m3d/M3DPosController.java
@@ -1,10 +1,15 @@
 package com.nhancv.facemask.m3d;
-
+import android.graphics.Rect;
+import android.os.Handler;
 import android.util.Log;
 
 import com.nhancv.facemask.FaceLandmarkListener;
+import com.nhancv.facemask.m3d.transformation.*;
 import com.tzutalin.dlib.VisionDetRet;
 
+import org.andresoviedo.android_3d_model_engine.model.Camera;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class M3DPosController implements FaceLandmarkListener {
@@ -12,15 +17,151 @@ public class M3DPosController implements FaceLandmarkListener {
     private final static String TAG = M3DPosController.class.getName();
     private M3DSurfaceView surfaceView;
     private M3DRenderer renderer;
+    private List<ObjectTransformation> listObjectTransformation;
+/*    private float previousX1 = 0;
+    private float previousY1 = 0;
+    private float dx1 = 0;
+    private float dy1 = 0;
+    */
+ /* final Runnable r = new Runnable() {
+                @Override
+                public void run() {
 
+                    i = i *-1;
+                    if(i==1) {
+                        //Translate(3, 0, 0);
+                        //degrees
+                        // Rotate(3, 0, 1, 0);
+                        Scale(160,160);
+
+                        //Scale(0.9f, 1, 1);
+                    }
+                    else{
+                        Scale(150,150);
+                        //Translate(-3, 0, 0);
+                        //Rotate(-3, 0, 1, 0);
+
+                    }
+                    requestRender();
+                    handler.postDelayed(this, 1000);
+                }
+                //Scale(1/0.9f, 1, 1);
+            };
+            handler.postDelayed(r,1000);*/
+    Handler handler = new Handler();
     public M3DPosController(M3DSurfaceView surfaceView) {
-        this.surfaceView = surfaceView;
+        this.surfaceView = surfaceView;//receive the current surface view
         this.renderer = surfaceView.getModelRenderer();
+        this.listObjectTransformation = new ArrayList<ObjectTransformation>();
     }
-
+    private int getHeight(VisionDetRet ret,float resizeRatio)
+    {
+        int h =(int) (Math.abs(ret.getBottom()-ret.getTop())*resizeRatio);
+        return h;
+    }
+    private int getWidth(VisionDetRet ret,float resizeRatio)
+    {
+        int w =(int) (Math.abs(ret.getRight()-ret.getLeft())*resizeRatio);
+        return w;
+    }
+    int i = -1;
+    int curArea = 22500;
     @Override
     public void landmarkUpdate(List<VisionDetRet> visionDetRetList) {
         Log.d(TAG, "landmarkUpdate: " + visionDetRetList.size());
+        //Translation
+        float ratio = 1.0f;
+        handler = new Handler();
+        listObjectTransformation = new ArrayList<>();
+        for (final VisionDetRet ret : visionDetRetList) {
 
+            float resizeRatio = 1.0f;
+            Rect bounds = new Rect();
+            bounds.left = (int) (ret.getLeft() * resizeRatio);
+            bounds.top = (int) (ret.getTop() * resizeRatio);
+            bounds.right = (int) (ret.getRight() * resizeRatio);
+            bounds.bottom = (int) (ret.getBottom() * resizeRatio);
+            int h = getHeight(ret,resizeRatio);
+            int w = getWidth(ret,resizeRatio);
+            //assume: (x,y) : (
+            //translateWorld(100,0);
+
+            //Scale(w,h);
+            // Draw landmark
+          /*  ArrayList<Point> landmarks = ret.getFaceLandmarks();
+            for (Point point : landmarks) {
+                int pointX = (int) (point.x * resizeRatio);
+                int pointY = (int) (point.y * resizeRatio);
+            }*/
+            ObjectTransformation objectTransformation;
+            i = i *-1;
+            if(i==1) {
+               // Translate(0, 1, 0);
+                //degrees
+                Rotation rotation = new Rotation(0,1,0);
+                Translation translation = new Translation(0,0,0);
+                //Scale scale = new Scale(1,1,1);
+                //objectTransformation = new ObjectTransformation(rotation,scale,translation);
+                //Scale(160,160);
+                //Scale(1, 1, 0.9f);
+                objectTransformation = new ObjectTransformationBuilder().setRotation(rotation)
+                                                                        .setTranslation(translation)
+                                                                        .build();
+            }
+            else{
+                //Scale(150,150);
+                //Translate(0, -1, 0);
+                Rotation rotation = new Rotation(0,-1,0);
+                Translation translation = new Translation(0,0,0);
+                //Scale scale = new Scale(1,1,1);
+                objectTransformation = new ObjectTransformationBuilder().setRotation(rotation)
+                        .setTranslation(translation)
+                        .build();
+                //Scale(1, 1, 1/0.9f);
+
+            }
+            if(objectTransformation!=null)
+                listObjectTransformation.add( objectTransformation);//add each tranformation for each object
+        }
+        renderer.setObjectTransformationList(listObjectTransformation);
+        requestRender();
     }
+     public void Scale(int w, int h) {
+        //zoom factor is the ratio of previous face and current detected face
+        int max = Math.max(this.renderer.getWidth(), this.renderer.getHeight()); //max of render vaule
+        M3DSceneLoader scene = this.surfaceView.getScene(); //get current scene
+        Camera camera = scene.getCamera();
+        float preArea = curArea;
+        curArea = w*h;
+        float zoomFactor = 1 ;
+        Log.d("M3DPosController", ""+zoomFactor);
+        camera.MoveCameraZ(zoomFactor);
+        //this.surfaceView.requestRender();
+    }
+    public void requestRender() {
+        this.surfaceView.requestRender();
+    }
+/*    public void Translate(float x,float y,float z) {
+        renderer.setTranslate(x,y,z);
+    }
+    public void Rotate(float angle,float x, float y, float z) {
+        renderer.setRotate(angle,x,y,z);
+    }
+    public void Scale(float x, float y, float z) {
+        renderer.setScale(x,y,z);
+    }*/
+/*    public void translateWorld(float x, float y){
+        dx1 = x - previousX1;
+        dy1 = y - previousY1;
+        int max = Math.max(this.renderer.getWidth(), this.renderer.getHeight());
+
+        M3DSceneLoader scene = this.surfaceView.getScene(); //get current scene
+        Camera camera = scene.getCamera();
+        //camera.translateCameraImpl(100, 100);
+        dx1 = (float)(dx1 / max * Math.PI * 2);
+        dy1 = (float)(dy1 / max * Math.PI * 2);
+        camera.translateCamera(dx1,dy1);
+
+        this.surfaceView.requestRender();
+    }*/
 }
--- app/src/main/java/com/nhancv/facemask/m3d/M3DRenderer.java
+++ app/src/main/java/com/nhancv/facemask/m3d/M3DRenderer.java
@@ -14,8 +14,10 @@ import org.andresoviedo.android_3d_model_engine.model.Object3D;
 import org.andresoviedo.android_3d_model_engine.model.Object3DData;
 import org.andresoviedo.android_3d_model_engine.services.Object3DBuilder;
 import org.andresoviedo.util.android.GLUtil;
+import com.nhancv.facemask.m3d.transformation.*;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,7 +67,7 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
      * Skeleton Animator
      */
     private Animator animator = new Animator();
-
+    private List<ObjectTransformation> objectTransformationList = new ArrayList<>();
     /**
      * Construct a new renderer for the specified surface view
      *
@@ -76,12 +78,36 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
         this.main = modelSurfaceView;
     }
 
+    public void setObjectTransformationList(List<ObjectTransformation> objectTransformationList) {
+        this.objectTransformationList = objectTransformationList;
+    }
+
     public float getNear() {
         return near;
     }
 
     public float getFar() {
         return far;
+    }
+    int value = -1;
+    public void setRotate(float angle,float x, float y, float z) {
+        Matrix.rotateM(modelProjectionMatrix, 0,angle, x, y, z);
+    }
+    public void setScale(float x, float y ,float z){
+        //default is 1, 1, 1
+        if(x ==0) {
+            x =1;
+        }
+        if(y == 0) {
+            y = 1;
+        }
+        if(z==0) {
+            z =1;
+        }
+        Matrix.scaleM(modelProjectionMatrix, 0, x, y, z);
+    }
+    public void setTranslate(float x, float y, float z) {
+       Matrix.translateM(modelProjectionMatrix, 0, x, y, z);
     }
 
     @Override
@@ -130,11 +156,23 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mvpMatrix, 0, modelProjectionMatrix, 0, modelViewMatrix, 0);
+
     }
 
 
-
-
+    int i = -1;
+    public void performTranformation(Object3DData objData,ObjectTransformation transformation)
+    {
+        float[] rotation = transformation.getRotationValue();
+        if(rotation!=null)
+            objData.setRotation(rotation);
+        float[] translation = transformation.getTranslationValue();
+        if(translation!=null)
+            objData.setPosition(translation);
+        float[] scale = transformation.getScaleValue();
+        if(scale!=null)
+            objData.setScale(scale);
+    }
     @Override
     public void onDrawFrame(GL10 unused) {
 
@@ -152,6 +190,7 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
 
         // recalculate mvp matrix according to where we are looking at now
         Camera camera = scene.getCamera();
+
         if (camera.hasChanged()) {
             Matrix.setLookAtM(modelViewMatrix, 0, camera.xPos, camera.yPos, camera.zPos, camera.xView, camera.yView,
                     camera.zView, camera.xUp, camera.yUp, camera.zUp);
@@ -161,7 +200,7 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
         }
 
         // draw light
-        if (scene.isDrawLighting()) {
+/*       if (scene.isDrawLighting()) {
 
             Object3DImpl lightBulbDrawer = (Object3DImpl) drawer.getPointDrawer();
 
@@ -172,17 +211,29 @@ public class M3DRenderer implements GLSurfaceView.Renderer  {
 
             // Draw a point that represents the light bulb
             lightBulbDrawer.draw(scene.getLightBulb(), modelProjectionMatrix, modelViewMatrix, -1, lightPosInEyeSpace);
-        }
-
+        }*/
+      /*  Object3DData axis = Object3DBuilder.buildAxis().setId("axis");
+        axis.setColor(new float[]{1.0f,0,0,1.0f});
+        scene.addObject(axis);*/
         List<Object3DData> objects = scene.getObjects();
         for (int i=0; i<objects.size(); i++) {
             Object3DData objData = null;
+            ObjectTransformation transformation = null;
             try {
                 objData = objects.get(i);
-                boolean changed = objData.isChanged();
+                if(objectTransformationList!=null && i<objectTransformationList.size()){
+                    transformation = objectTransformationList.get(i);
+                    //perform tranformation with objectData using tranformation
+                    performTranformation(objData, transformation);
+                }
+                //objData.setColor(new float[]{1.0f,0,0,1.0f});
+                //objData.setPosition(new float[]{0,1.0f,0});
+                //objData.setRotation()
+                //objData.setScale(new float[]{1,1,1});
+                boolean changed = objData.isChanged ();
 
                 Object3D drawerObject = drawer.getDrawer(objData, scene.isDrawTextures(), scene.isDrawLighting(),
-                        scene.isDrawAnimation());
+                        scene.isDrawAnimation()); //obj
 
                 if (!infoLogged) {
                     Log.i("ModelRenderer","Using drawer "+drawerObject.getClass());
