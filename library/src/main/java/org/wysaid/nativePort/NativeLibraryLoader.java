4afc057a06ccc58874995f4689f0239271a3c15f
Nhan Cao <nhancv92@gmail.com>
2019-07-09 13:03:02 +0700

    integrate gpuimage-plus

---
+++ library/src/main/java/org/wysaid/nativePort/NativeLibraryLoader.java
@@ -0,0 +1,15 @@
+package org.wysaid.nativePort;
+
+/**
+ * Created by wangyang on 15/7/30.
+ */
+public class NativeLibraryLoader {
+
+    public static void load() {
+//        System.loadLibrary("ffmpeg");
+        System.loadLibrary("CGE");
+//        System.loadLibrary("CGEExt");
+//        CGEFFmpegNativeLibrary.avRegisterAll();
+    }
+
+}
